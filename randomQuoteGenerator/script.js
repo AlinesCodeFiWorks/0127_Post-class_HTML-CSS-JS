c61f431adac99f0fead44389d615c922f1a9a86f
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-27 18:34:39 -0600

    Concluded random quote generator

--- randomQuoteGenerator/script.js
+++ randomQuoteGenerator/script.js
@@ -1,1 +1,8 @@
+const quotes = ["Quote 1", "Quote 2", "Quote 3", "Quote 4", "Quote 5"];
 
+document.getElementById("generate-quote").addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById(
+    "quote-display"
+  ).innerText = `"${quotes[randomIndex]}"`;
+});
