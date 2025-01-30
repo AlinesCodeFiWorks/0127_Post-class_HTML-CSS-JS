ca447cb439890b053a71a058c562dc3000275db3
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-29 23:42:26 -0600

    Started setting up for Exercise 5, still troubleshooting

--- exercises4-6/script.js
+++ exercises4-6/script.js
@@ -9,3 +9,16 @@ function displayText() {
   inputDisplay.textContent = myInput.value;
 }
 myButton.addEventListener("click", displayText);
+
+//Exercise 5
+//Ingredients
+const countDown = setInterval(10);
+const countDownTimer = document.querySelector(".countDownTimer");
+const startButton = document.querySelector(".startButton");
+const stopButton = document.querySelector(".stopButton");
+//Recipe
+function startTimer{}
+function stopTimer{countDown.clearInterval();}
+countDownTimer.textContent=countDown;
+startButton.addEventListener('click',startTimer);
+stopButton.addEventListener('click',stopTimer);
